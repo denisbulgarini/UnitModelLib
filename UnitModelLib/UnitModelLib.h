560437b7145afc24d23b0f7a4245188243939819
Denis, Bulgarini <d.bulgarini@dasgip.de>
2012-10-10 13:51:33 +0200

    Initial Commit

---
+++ UnitModelLib/UnitModelLib.h
@@ -0,0 +1,13 @@
+//
+//  UnitModelLib.h
+//  UnitModelLib
+//
+//  Created by Denis, Bulgarini on 10.10.12.
+//  Copyright (c) 2012 Denis, Bulgarini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnitModelLib : NSObject
+
+@end
---
+++ UnitModelLib/UnitModelLib.m
@@ -0,0 +1,13 @@
+//
+//  UnitModelLib.m
+//  UnitModelLib
+//
+//  Created by Denis, Bulgarini on 10.10.12.
+//  Copyright (c) 2012 Denis, Bulgarini. All rights reserved.
+//
+
+#import "UnitModelLib.h"
+
+@implementation UnitModelLib
+
+@end
