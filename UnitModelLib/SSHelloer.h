290bf1d8ba762bcef7240249f7dd8c20a1fecdf3
Denis, Bulgarini <d.bulgarini@dasgip.de>
2012-10-10 14:56:37 +0200

    fist commit

---
+++ UnitModelLib/SSHelloer.h
@@ -0,0 +1,13 @@
+//
+//  SSHelloer.h
+//  UnitModelLib
+//
+//  Created by Denis, Bulgarini on 10.10.12.
+//  Copyright (c) 2012 Denis, Bulgarini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSHelloer : NSObject
+
+@end
---
+++ UnitModelLib/SSHelloer.m
@@ -0,0 +1,13 @@
+//
+//  SSHelloer.m
+//  UnitModelLib
+//
+//  Created by Denis, Bulgarini on 10.10.12.
+//  Copyright (c) 2012 Denis, Bulgarini. All rights reserved.
+//
+
+#import "SSHelloer.h"
+
+@implementation SSHelloer
+
+@end
